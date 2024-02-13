ef7e2055a01eec68de44b5016f1bebee613ccab6
jhalakdhant <154356815+jhalakdhant@users.noreply.github.com>
2024-02-13 02:00:28 +0000

    data types learning

---
+++ 01_basics/daatypes.js
@@ -0,0 +1,12 @@
+"use strict";// treat all JS code as newer version
+//data types in java scripts 
+let Name="jhalak"
+let age=20
+ console.log(Name,age)
+ //numbers 
+ //bigint phone numbers 
+ //string
+ //null - its an object 
+ //undefined value is not defined 
+ //boolean- to specify true /false
+ //
